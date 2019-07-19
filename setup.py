006c4ce8cf940421883bc4ccadbf197d76ba3de2
vangheem <vangheem@gmail.com>
2019-07-19 08:40:35 -0400

    Back to development: 1.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='pytest-docker-fixtures',
-    version='1.3.1',
+    version='1.3.2.dev0',
     description='pytest docker fixtures',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGELOG.rst').read()),
